5630652e051a72c1763fc83ac3315994aa4442ad
eXt-Ra <vanardois.romain@gmail.com>
2015-09-23 17:04:01 +0200

    Ajout Btn supprimer message top listview

--- ActivityChat.cs
+++ ActivityChat.cs
@@ -62,11 +62,91 @@ namespace DMSvStandard
 				});
 			}
 
-
+			View view = LayoutInflater.From (this).Inflate (Resource.Layout.ListeViewDelete, null, false);
+			mListView.AddHeaderView (view);
+			view.Click += Btndeletemsg_Click;
 			MessageBoxAdapter adapter = new MessageBoxAdapter (this, mItems);
 			mListView.Adapter = adapter;
 			//mListView.ItemClick += MListView_ItemClick;
 			//mListView.ItemLongClick += MListView_ItemLongClick;
+
+			//EDITTEXT
+			var btnsend = FindViewById<Button>(Resource.Id.btnsend);
+			btnsend.Click += Btnsend_Click;
+
+
+
+
+		}
+
+		void  Btnsend_Click (object sender, EventArgs e){
+
+			DBRepository dbr = new DBRepository ();
+			var newmessage = FindViewById<TextView>(Resource.Id.editnewmsg);
+			var resinteg = dbr.InsertDataMessage ("",newmessage.Text,3,DateTime.Now,2);
+
+			string dbPath = System.IO.Path.Combine (System.Environment.GetFolderPath
+				(System.Environment.SpecialFolder.Personal), "ormDMS.db3");
+			var db = new SQLiteConnection (dbPath);
+
+
+			var table = db.Query<Message> ("SELECT * FROM Message");
+
+
+			foreach (var item in table) {
+
+				mItems.Add (new Message () {
+					texte = item.texte,
+					utilisateurAndsoft = item.utilisateurAndsoft,
+					statut = item.statut,
+					datemessage = item.datemessage,
+					typemsg = item.typemsg,
+					Id = item.Id
+				});
+			}
+
+
+			MessageBoxAdapter adapter = new MessageBoxAdapter (this, mItems);
+			mListView.Adapter = adapter;
+
+
+			
+		}
+
+
+		void  Btndeletemsg_Click (object sender, EventArgs e){
+
+			DBRepository dbr = new DBRepository ();
+			var newmessage = FindViewById<TextView>(Resource.Id.editnewmsg);
+			var resinteg = dbr.InsertDataMessage ("",newmessage.Text,3,DateTime.Now,2);
+
+			string dbPath = System.IO.Path.Combine (System.Environment.GetFolderPath
+				(System.Environment.SpecialFolder.Personal), "ormDMS.db3");
+			var db = new SQLiteConnection (dbPath);
+
+			var del = db.Query<Message> ("DELETE FROM Message");
+
+			var table = db.Query<Message> ("SELECT * FROM Message");
+
+
+			foreach (var item in table) {
+
+				mItems.Add (new Message () {
+					texte = item.texte,
+					utilisateurAndsoft = item.utilisateurAndsoft,
+					statut = item.statut,
+					datemessage = item.datemessage,
+					typemsg = item.typemsg,
+					Id = item.Id
+				});
+			}
+
+
+			MessageBoxAdapter adapter = new MessageBoxAdapter (this, mItems);
+			mListView.Adapter = adapter;
+
+
+
 		}
 	}
 }
